5f8efe7e1f872276d11f085b1a644a259dd8c4bb
zwei76 <ericlin8840461@hotmail.com>
2018-04-15 11:12:09 +0800

    add

--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -12,21 +12,3 @@ function insertionSort(_arr) {
   }
   return arr;
 }
-
-function binaryInsertionSort(_arr) {
-  const arr = [].slice.call(_arr);
-  const len = arr.length;
-  for (let i = 1; i < len; i += 1) {
-    let f = i;
-    const temp = arr[i];
-    const low = 0;
-    const high = i - 1;
-    const min = Math.floor(low + high) / 2;
-    while (f > 0 && arr[f - 1] > temp) {
-      arr[f] = arr[f - 1];
-      f -= 1;
-    }
-    arr[f] = temp;
-  }
-  return arr;
-}
